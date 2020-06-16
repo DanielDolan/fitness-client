e30efad01a6ee068a7fd736e9f5e479efc298f0d
Dante Angelone <dante31@Dantes-MBP.home>
2020-06-16 10:41:13 -0400

    api link works

--- src/app/App.js
+++ src/app/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+//import SearchBar from "./Components/SearchBar";
+import { debounce } from "lodash";
+import request from "superagent";
+
+const apiKey = process.env.REACT_APP_API_KEY;
+const apiID = process.env.REACT_APP_API_ID;
+const mealsHome = "https://api.edamam.com/search?q=chicken&app_id="+apiID+"&app_key="+apiKey+"&from=0&to=3&calories=591-722&health=alcohol-free"
+const test = "https://api.edamam.com/search?q=chicken&app_id=b4ec86bf&app_key=331d54a3fbc80d1d2f09d263a2aa2e16&from=0&to=3&calories=591-722&health=alcohol-free"
+
 
 function App() {
+
+  console.log(mealsHome)
+  console.log(test)
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+  
     </div>
   );
 }
