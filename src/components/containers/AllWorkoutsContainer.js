fa614ab56a20069bad06cec4b1f3cfdd38ede5c4
Nicole <nwoje13@gmail.com>
2020-06-16 10:17:03 -0400

    added workouts

---
+++ src/components/containers/AllWorkoutsContainer.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchAllWorkoutsThunk } from '../../thunks';
+import { AllWorkoutsView } from '../views';
+
+// Smart container;
+class AllWorkoutsContainer extends Component {
+  componentDidMount() {
+    this.props.fetchAllWorkouts();
+  }
+
+  render() {
+    return <AllWorkoutsView allWorkouts={this.props.allWorkouts} />
+  }
+}
+
+// Map state to props;
+const mapState = state => {
+  return {
+    allWorkouts: state.allWorkouts
+  }
+}
+
+// Map dispatch to props;
+const mapDispatch = dispatch => {
+  return {
+    fetchAllWorkouts: () => dispatch(fetchAllWorkoutsThunk())
+  }
+}
+
+// Type check props;
+AllWorkoutsContainer.propTypes = {
+  allWorkouts: PropTypes.array.isRequired,
+  fetchAllWorkouts: PropTypes.func.isRequired
+}
+
+// Export our store-connected container by default;
+export default connect(mapState, mapDispatch)(AllWorkoutsContainer);
--- src/components/routes/RoutesView.jsx
+++ src/components/routes/RoutesView.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import {
   Home,
+  AllWorkoutsContainer,
 } from "../containers";
 
 const RoutesView = () => {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
+      {/* <Route exact path="/" component={Home} /> */}
+      <Route exact path="/" component={AllWorkoutsContainer} />
 
     </Switch>
   );
