df884a62960755c15bd0260651bb2e1b9094b40e
Summer <tiantianxia@MacBook-Pro.local>
2020-06-20 14:02:11 -0400

    modify redirect page

--- src/components/containers/ProfilePage.js
+++ src/components/containers/ProfilePage.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { me } from "../../thunks";
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom'
 // import AuthFormContainer from './AuthFormContainer'
 
 class ProfilePage extends Component {
@@ -8,14 +11,19 @@ class ProfilePage extends Component {
       email: "",
       password: "",
       lastName: "",
-      firstName: ""
+      firstName: "",
+      rediretLogin: false
     }
+  }
+  componentDidMount() {
+    this.props.me();
+    console.log(this.props.firstName);
   }
 
   render() {
     return (
       <div>
-        {/* <h1>{userEmail}</h1> */}
+        <h1>{this.props.firstName}</h1>
       </div>
     );
   }
@@ -25,4 +33,21 @@ class ProfilePage extends Component {
 ProfilePage.propTypes = {
 };
 
-export default ProfilePage;
+const mapUser = state => {
+  return {
+    name: "user",
+    displayName: "User",
+    error: state.user.error,
+    firstName: state.user.firstName,
+    isLoggedIn: !!state.user.id
+  };
+};
+
+// Map login dispatch to props;
+const mapDispatchUser = dispatch => {
+  return { 
+    me: () => dispatch(me())
+  }
+};
+
+export default connect(mapUser, mapDispatchUser)(ProfilePage);
